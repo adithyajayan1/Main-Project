a6614e6dfed3a84df4f08d53efa11e1342c3023e
adithyajayan1 <adithyajayan961@gmail.com>
2026-03-23 14:22:56 +0530

    Remove unused files

--- download_model.py
+++
@@ -1,4 +0,0 @@
-import urllib.request
-url = "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task"
-urllib.request.urlretrieve(url, "pose_landmarker.task")
-print("Done!")
